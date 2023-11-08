5a80073ff894221903c87058a23bd2cf5e8661c6
deevinefayvour <oghifodivine@gmail.com>
2023-11-08 19:47:58 +0100

    Adding a new version

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello, this is a dummy app!\n');
 });
 
-//WE are making changes to port 5000 from 3000
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
